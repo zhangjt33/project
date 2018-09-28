82fd0735a71a919e235e1d8c31bb7438b6da7c28
npathcheck <1179128392@qq.com>
2018-09-28 23:15:42 +0800

    建立项目

---
+++ input_data.py
@@ -0,0 +1,17 @@
+import re
+
+def read(file_name):
+    '''
+    :param file_name: 需要读入的文件名
+    :return: 以二维列表形式存储的单词
+    以逗号，空格，括号分隔字符串
+    '''
+    texts = []
+    file_object = open(file_name, 'r', encoding='UTF-8')
+    for line in file_object.readlines():
+        texts.append(list(filter(None, (word.lower() for word in re.split(r'[(),. ]', line.strip())))))
+    file_object.close()
+    return texts
+
+if __name__ == "__main__":
+    print(read("data/2/trainData.txt"))

4712e6f66ebb154f3f96f469d58fda89f6456037
zhangjintao <gdzhangjintao@163.com>
2018-09-29 00:03:42 +0800

    make a test by jintao

--- input_data.py
+++ input_data.py
@@ -1,5 +1,6 @@
 import re
 
+
 def read(file_name):
     '''
     :param file_name: 需要读入的文件名
@@ -9,9 +10,14 @@ def read(file_name):
     texts = []
     file_object = open(file_name, 'r', encoding='UTF-8')
     for line in file_object.readlines():
-        texts.append(list(filter(None, (word.lower() for word in re.split(r'[(),. ]', line.strip())))))
+        texts.append(
+            list(
+                filter(None,
+                       (word.lower()
+                        for word in re.split(r'[(),. ]', line.strip())))))
     file_object.close()
     return texts
+
 
 if __name__ == "__main__":
     print(read("data/2/trainData.txt"))
